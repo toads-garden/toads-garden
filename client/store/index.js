186114e2fa2eeca6a747c76a33f1ffd95b1c0df0
marcela <marcela.alonso.s@gmail.com>
2023-01-25 17:18:25 -0500

    changes to file structure, pckage.json now uses parcel to bundle, new sprite

--- client/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
-
-const store = configureStore({
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-});
-
-export default store;
