0985c8de929b2fe5feb847a2fe6f6c0e918e6b39
Lindsay Powell <lindsayerin3393@gmail.com>
2023-01-25 14:49:41 -0500

    add scenes folder

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import * as Phaser from "phaser";
+
+const config = {
+  type: Phaser.AUTO,
+  dom: { createContainer: true },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "game",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: innerWidth,
+    height: innerHeight,
+  },
+  scene: [],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+
+  render: {
+    pixelArt: true,
+  },
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/underwater.js
@@ -0,0 +1,14 @@
+import { Scene } from "phaser";
+
+class Underwater extends Scene {
+  constructor() {
+    super("Underwater");
+  }
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
+
+export default Underwater;
